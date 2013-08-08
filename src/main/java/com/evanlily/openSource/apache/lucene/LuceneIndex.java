c45dbe71a5e1377a76dec43354a75df44a8e33c6
yimi8126 <evan8126@gmail.com>
2013-08-08 17:03:28 +0800

    commit test

--- src/main/java/com/evanlily/openSource/apache/lucene/LuceneIndex.java
+++ src/main/java/com/evanlily/openSource/apache/lucene/LuceneIndex.java
@@ -4,7 +4,7 @@ package com.evanlily.openSource.apache.lucene;
  * User: 一米
  * Date: 13-7-29
  * Time: 下午4:52
- * Description:
+ * Description:lucene 创建索引
  */
 public class LuceneIndex {
     public static void main(String[] args) {
